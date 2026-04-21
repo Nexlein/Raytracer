e3faeca8802fb942b4d4e791e48eb06354822bc9
Nielsen Combe-Bracciale <nielsen.combe-bracciale@epitech.eu>
2026-04-21 21:24:12 +0200

    Add PrimitiveFactory class for creating sphere primitives from configuration settings

---
+++ RayTracer/include/Factory/PrimitiveFactory.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2026
+** Raytracer
+** File description:
+** PrimitiveFactory
+*/
+
+/// @file PrimitiveFactory.hpp
+/// @brief Factory for creating primitives
+
+#pragma once
+
+#include <libconfig.h++>
+#include <memory>
+#include <string>
+
+#include "IPrimitive.hpp"
+
+/// @brief Namespace for the Ray Tracer project
+namespace RayTracer {
+    /// @brief Factory class for creating primitives from configuration settings
+    class PrimitiveFactory {
+        public:
+        /// @brief Creates a sphere primitive from the given configuration setting
+        /// @param setting The configuration setting containing the parameters for the sphere
+        /// @return A unique pointer to the created sphere primitive
+        [[nodiscard]] std::unique_ptr<IPrimitive> createSphere(const libconfig::Setting& setting);
+    };
+}  // namespace RayTracer
