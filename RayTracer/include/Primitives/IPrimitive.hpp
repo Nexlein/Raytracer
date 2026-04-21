207538845e458edff22ea21424e27e04c504165f
Nielsen Combe-Bracciale <nielsen.combe-bracciale@epitech.eu>
2026-04-21 21:23:48 +0200

    Refactor IPrimitive and Sphere classes to include HitRecord in hits method for improved ray intersection handling

--- RayTracer/include/Primitives/IPrimitive.hpp
+++ RayTracer/include/Primitives/IPrimitive.hpp
@@ -12,16 +12,32 @@
 
 #include "Ray.hpp"
 
-/// @brief Namespace for the RayTracer project
+/// @brief Namespace for the Ray Tracer project
 namespace RayTracer {
+    /// @brief Struct to hold hit record information when a ray intersects a primitive
+    struct HitRecord {
+        /// @brief Distance from the ray origin to the hit point
+        double distance;
+        /// @brief Point of intersection in 3D space
+        Math::Point3D<double> p;
+        /// @brief Normal vector at the point of intersection
+        Math::Vector3D<double> normal;
+    };
+
     /// @brief Interface for geometric primitives that can be intersected by rays
     class IPrimitive {
         public:
         virtual ~IPrimitive() = default;
 
-        /// @brief Determines if a ray hits the primitive
+        /// @brief Color of the primitive, used for rendering
+        Math::Vector3D<double> color;
+
+        /// @brief Determines if a ray intersects the primitive and fills the hit record with
+        /// intersection details
         /// @param ray The ray to test for intersection with the primitive
-        /// @return True if the ray intersects with the primitive, false otherwise
-        [[nodiscard]] virtual bool hits(const Ray& ray) const = 0;
+        /// @param hitRecord The hit record to be filled with intersection details if the ray hits the
+        /// primitive
+        /// @return True if the ray intersects the primitive, false otherwise
+        [[nodiscard]] virtual bool hits(const Ray& ray, HitRecord& hitRecord) const = 0;
     };
 }  // namespace RayTracer
