420937227ba2dead91e3bf4fef65b8ac9143b977
Nielsen Combe-Bracciale <nielsen.combe-bracciale@epitech.eu>
2026-04-21 21:10:56 +0200

    Refactor Camera class: replace Rectangle3D with position and rotation parameters, remove Rectangle3D class

--- RayTracer/include/Core/Camera.hpp
+++ RayTracer/include/Core/Camera.hpp
@@ -9,18 +9,32 @@
 
 #include "Point3D.hpp"
 #include "Ray.hpp"
-#include "Rectangle3D.hpp"
+#include "Vector3D.hpp"
 
 namespace RayTracer {
-    using Point3D = Math::Point3D<double>;
-
     class Camera {
         public:
-        Camera(const Point3D& origin, const Rectangle3D& screen);
-
-        Point3D _origin;
-        Rectangle3D _screen;
+        /// @brief Construct a Camera object
+        /// @param position Position of the camera in 3D space
+        /// @param rotation Rotation of the camera in degrees
+        /// @param width Width of the camera's viewport in pixels
+        /// @param height Height of the camera's viewport in pixels
+        /// @param fieldOfView Field of view of the camera in degrees
+        Camera(const Math::Point3D<double>& position, const Math::Vector3D<double>& rotation,
+               int width, int height, double fieldOfView);
 
         [[nodiscard]] Ray ray(double u, double v) const;
+
+        private:
+        /// @brief Position of the camera in 3D space
+        Math::Point3D<double> _position;
+        /// @brief Rotation of the camera in degrees
+        Math::Vector3D<double> _rotation;
+        /// @brief Width of the camera's viewport in pixels
+        int _width;
+        /// @brief Height of the camera's viewport in pixels
+        int _height;
+        /// @brief Field of view of the camera in degrees
+        double _fieldOfView;
     };
 }  // namespace RayTracer
--- RayTracer/source/Core/Camera.cpp
+++ RayTracer/source/Core/Camera.cpp
@@ -7,12 +7,28 @@
 
 #include "Camera.hpp"
 
-RayTracer::Camera::Camera(const Point3D& origin, const Rectangle3D& screen)
-    : _origin(origin), _screen(screen)
+#include <cmath>
+
+RayTracer::Camera::Camera(const Math::Point3D<double>& position,
+                          const Math::Vector3D<double>& rotation, int width, int height, double fov)
+    : _position(position), _rotation(rotation), _width(width), _height(height), _fieldOfView(fov)
 {
 }
 
 RayTracer::Ray RayTracer::Camera::ray(double u, double v) const
 {
-    return Ray(_origin, _screen.pointAt(u, v) - _origin);
+    double ratio = static_cast<double>(_width) / _height;
+
+    double scale = std::tan(_fieldOfView * 0.5 * M_PI / 180.0);
+
+    double px = (2.0 * u - 1.0) * ratio * scale;
+    double py = (2.0 * v - 1.0) * scale;
+
+    Math::Vector3D<double> direction(px, py, 1.0);
+
+    direction.rotateX(_rotation._x);
+    direction.rotateY(_rotation._y);
+    direction.rotateZ(_rotation._z);
+
+    return Ray(_position, direction);
 }
