5f05d61b0dd8d59e5009b81e5c91a09ea5a98512
Nielsen Combe-Bracciale <nielsen.combe-bracciale@epitech.eu>
2026-04-28 10:41:49 +0200

    add(AmbientLight): implement init method for configuration settings and rename computeDiffuse to computeLight

--- include/Light/AmbientLight.hpp
+++ include/Light/AmbientLight.hpp
@@ -12,14 +12,27 @@
 
 namespace RayTracer {
     class AmbientLight : public ILight {
-    public:
+        public:
+        /// @brief Default constructor for AmbientLight
+        AmbientLight() = default;
+
         /// @brief Constructor for AmbientLight
         /// @param intensity intensity of the light
         /// @param color color of the light
         AmbientLight(double intensity, Math::Vector3D<double> color);
 
-        [[nodiscard]] Math::Vector3D<double> computeDiffuse([[maybe_unused]] const HitRecord& hit) const override;
-
+        /// @brief Checks if the light casts shadows
+        /// @return True if the light casts shadows, false otherwise
         bool castsShadow() const override;
+
+        /// @brief Initializes the light with settings from a configuration file
+        /// @param setting The configuration settings for the light
+        void init(const libconfig::Setting& setting) override;
+
+        /// @brief Color of the light, used for rendering
+        /// @param hit The hit record for which to compute the light
+        /// @return Retourne la couleur de la lumière
+        [[nodiscard]] Math::Vector3D<double> computeLight(
+            [[maybe_unused]] const HitRecord& hit) const override;
     };
-}
+}  // namespace RayTracer
