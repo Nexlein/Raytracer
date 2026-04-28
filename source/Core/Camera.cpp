657fc0d504f4907cf617693eefd1d3e177f8fa49
Nielsen Combe-Bracciale <nielsen.combe-bracciale@epitech.eu>
2026-04-28 19:03:40 +0200

    feat(Camera): inline constructor and ray method for improved performance

--- source/Core/Camera.cpp
+++ source/Core/Camera.cpp
@@ -10,16 +10,7 @@
 
 #include "Camera.hpp"
 
-#include <cmath>
-
 #include "ConfigUtils.hpp"
-#include "RayTracerException.hpp"
-
-RayTracer::Camera::Camera(const Math::Point3D<double>& position,
-                          const Math::Vector3D<double>& rotation, double fov)
-    : _position(position), _rotation(rotation), _fieldOfView(fov)
-{
-}
 
 void RayTracer::Camera::init(const libconfig::Setting& setting)
 {
@@ -43,20 +34,4 @@ void RayTracer::Camera::init(const libconfig::Setting& setting)
 
     _fieldOfView = 90.0;
     ConfigUtils::getAsDouble(setting, "fieldOfView", _fieldOfView);
-}
-
-RayTracer::Ray RayTracer::Camera::ray(double u, double v, double ratio) const
-{
-    double scale = std::tan(_fieldOfView * 0.5 * M_PI / 180.0);
-
-    double px = (2.0 * u - 1.0) * ratio * scale;
-    double py = (2.0 * v - 1.0) * scale;
-
-    Math::Vector3D<double> direction(px, py, 1.0);
-
-    direction.rotateX(_rotation._x);
-    direction.rotateY(_rotation._y);
-    direction.rotateZ(_rotation._z);
-
-    return Ray(_position, direction);
 }
