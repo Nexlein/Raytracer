690927a00f4d31a461c4cc6da322710044473f9d
Nielsen Combe-Bracciale <nielsen.combe-bracciale@epitech.eu>
2026-04-28 10:41:11 +0200

    add(Plane): update constructor to use double for position and implement init method for configuration settings

--- include/Primitives/Plane.hpp
+++ include/Primitives/Plane.hpp
@@ -10,9 +10,10 @@
 
 #pragma once
 
+#include <string>
+
 #include "IPrimitive.hpp"
 #include "Point3D.hpp"
-#include <string>
 
 /// @brief Namespace for the RayTracer project
 namespace RayTracer {
@@ -25,18 +26,24 @@ namespace RayTracer {
         /// @brief Constructs a plane object
         /// @param axis The axis along which the plane is oriented (X, Y, or Z)
         /// @param position The position of the plane along the specified axis
-        Plane(std::string axis, int position);
+        Plane(std::string axis, double position);
 
         /// @brief Normal vector of the plane
         Vector3D _normal;
 
         /// @brief Position of the plane in 3D space
-        int _position;
+        double _position;
 
-        /// @brief Determines if a ray intersects the plane and fills the hit record with intersection details
+        /// @brief Determines if a ray intersects the plane and fills the hit record with
+        /// intersection details
         /// @param ray The ray to test for intersection with the plane
-        /// @param hitRecord The hit record to be filled with intersection details if the ray hits the plane
+        /// @param hitRecord The hit record to be filled with intersection details if the ray hits
+        /// the plane
         /// @return True if the ray intersects the plane, false otherwise
         [[nodiscard]] bool hits(const Ray& ray, HitRecord& hitRecord) const override;
+
+        /// @brief Initializes the plane with settings from a configuration file
+        /// @param setting The configuration settings for the plane
+        void init(const libconfig::Setting& setting) override;
     };
-}
+}  // namespace RayTracer
