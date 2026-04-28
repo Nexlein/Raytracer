3fbc8fec4d2f01599392039f44c1dc7c4353de39
Yumi-alt <julie.vincent@epitech.eu>
2026-04-28 17:57:46 +0200

    feat: cylinder limited and infinite

---
+++ include/Primitives/Cylinder.hpp
@@ -0,0 +1,55 @@
+/*
+** EPITECH PROJECT, 2025
+** Raytracer
+** File description:
+** Cylinder.hpp
+*/
+
+/// @file Cylinder.hpp
+/// @brief Cylinder class definition
+
+#pragma once
+
+#include "IPrimitive.hpp"
+#include "Point3D.hpp"
+#include <optional>
+
+/// @brief Namespace for the RayTracer project
+namespace RayTracer {
+    using Point3D = Math::Point3D<double>;
+    using Vector3D = Math::Vector3D<double>;
+
+    /// @brief Represents a Cylinder in 3D space
+    class Cylinder : public IPrimitive {
+        public:
+        Cylinder() = default;
+        /// @brief Constructs a cylinder object
+        /// @param base The base point of the cylinder in 3D space
+        /// @param axis The orientation of the cylinder
+        /// @param radius The radius of the cylinder
+        /// @param height The height of the cylinder
+        /// if the height isn't set, it will be considered as an infinite heigth
+        Cylinder(const Point3D& base, Vector3D& axis, double radius, std::optional<double> height);
+
+        /// @brief Center of the cylinder's base
+        Point3D _base;
+        /// @brief Orientation of the cylinder
+        Vector3D _axis;
+        /// @brief Radius of the cylinder
+        double _radius;
+        /// @brief height The height of the cylinder
+        std::optional<double> _height;
+
+        /// @brief Determines if a ray intersects the cylinder and fills the hit record with
+        /// intersection details
+        /// @param ray The ray to test for intersection with the cylinder
+        /// @param hitRecord The hit record to be filled with intersection details if the ray hits
+        /// the cylinder
+        /// @return True if the ray intersects the cylinder, false otherwise
+        [[nodiscard]] bool hits(const Ray& ray, HitRecord& hitRecord) const override;
+
+        /// @brief Initializes the cylinder with settings from a configuration file
+        /// @param setting The configuration settings for the cylinder
+        void init(const libconfig::Setting& setting) override;
+    };
+}  // namespace RayTracer
