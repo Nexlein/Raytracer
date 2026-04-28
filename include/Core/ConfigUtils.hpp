bb19faa873ce9191b370aa75d520c1109232b70e
Nielsen Combe-Bracciale <nielsen.combe-bracciale@epitech.eu>
2026-04-28 11:36:51 +0200

    fix(ConfigUtils): change key parameter type in getAsDouble to std::string for consistency

--- include/Core/ConfigUtils.hpp
+++ include/Core/ConfigUtils.hpp
@@ -19,21 +19,21 @@ namespace RayTracer {
         /// @param key The key of the value to extract
         /// @param value The variable to store the extracted value
         /// @return True if the value was successfully extracted, false otherwise
-        inline bool getAsDouble(const libconfig::Setting& setting, const char* key, double& value)
+        inline bool getAsDouble(const libconfig::Setting& setting, const std::string& key, double& value)
         {
             int intValue = 0;
-            if (setting.lookupValue(key, intValue)) {
+            if (setting.lookupValue(key.c_str(), intValue)) {
                 value = static_cast<double>(intValue);
                 return true;
             }
 
             float floatValue = 0.0f;
-            if (setting.lookupValue(key, floatValue)) {
+            if (setting.lookupValue(key.c_str(), floatValue)) {
                 value = static_cast<double>(floatValue);
                 return true;
             }
 
-            return setting.lookupValue(key, value);
+            return setting.lookupValue(key.c_str(), value);
         }
     }  // namespace ConfigUtils
 }  // namespace RayTracer
