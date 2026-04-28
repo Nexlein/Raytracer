9aa1e620d9d07d7e05b2f3b7ac72c02097117a6d
Nielsen Combe-Bracciale <nielsen.combe-bracciale@epitech.eu>
2026-04-28 19:02:44 +0200

    feat(Point3D): inline method implementations for improved performance

--- include/Math/Point3D.hpp
+++ include/Math/Point3D.hpp
@@ -23,7 +23,7 @@ namespace Math {
         /// @param x The x-coordinate of the point (default is 0)
         /// @param y The y-coordinate of the point (default is 0)
         /// @param z The z-coordinate of the point (default is 0)
-        Point3D(T x = T{}, T y = T{}, T z = T{});
+        Point3D(T x = T{}, T y = T{}, T z = T{}) : _x(x), _y(y), _z(z) {}
 
         /// @brief The x-coordinate of the point
         T _x;
@@ -37,27 +37,44 @@ namespace Math {
         /// @brief Adds a vector to the point, resulting in a new point
         /// @param other The vector to be added to the point
         /// @return A new Point3D object representing the result of adding the vector to the point
-        [[nodiscard]] Point3D operator+(const Vector3D<T>& other) const;
+        [[nodiscard]] Point3D operator+(const Vector3D<T>& other) const
+        {
+            return Point3D(_x + other._x, _y + other._y, _z + other._z);
+        }
 
         /// @brief Subtracts a vector from the point, resulting in a new point
         /// @param other The vector to be subtracted from the point
         /// @return A new Point3D object representing the result of subtracting the vector from the
         /// point
-        [[nodiscard]] Point3D operator-(const Vector3D<T>& other) const;
+        [[nodiscard]] Point3D operator-(const Vector3D<T>& other) const
+        {
+            return Point3D(_x - other._x, _y - other._y, _z - other._z);
+        }
 
         /// @brief Subtracts another point from this point, resulting in a vector
         /// @param other The point to be subtracted from this point
         /// @return A new Vector3D object representing the result of subtracting the other point
         /// from this point
-        [[nodiscard]] Math::Vector3D<T> operator-(const Point3D& other) const;
+        [[nodiscard]] Math::Vector3D<T> operator-(const Point3D& other) const
+        {
+            return Math::Vector3D<T>(_x - other._x, _y - other._y, _z - other._z);
+        }
 
         /// @brief Computes the dot product of this point and another vector
         /// @param other The other vector
         /// @return The dot product
-        [[nodiscard]] T dot(const Vector3D<T>& other) const;
+        [[nodiscard]] T dot(const Vector3D<T>& other) const
+        {
+            return _x * other._x + _y * other._y + _z * other._z;
+        }
 
         /// @brief Translates the point by a given vector, modifying the point's coordinates
         /// @param v The vector by which to translate the point
-        void translate(const Vector3D<T>& v);
+        void translate(const Vector3D<T>& v)
+        {
+            _x += v._x;
+            _y += v._y;
+            _z += v._z;
+        }
     };
 }  // namespace Math
