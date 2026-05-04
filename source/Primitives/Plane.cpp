7203715722c1efe061cbb3945530328c045d2459
Nielsen Combe-Bracciale <nielsen.combe-bracciale@epitech.eu>
2026-05-04 13:03:13 +0200

    feat(plane): enhance UV mapping calculations in hits method

--- source/Primitives/Plane.cpp
+++ source/Primitives/Plane.cpp
@@ -31,6 +31,26 @@ bool RayTracer::Plane::hits(const Ray& ray, HitRecord& rec) const
     rec.distance = t;
     rec.p = ray._origin + (ray._direction * t);
     rec.normal = _normal;
+
+    // UV Mapping
+    Math::Vector3D<double> edgeDirectionX(1, 0, 0);
+    if (std::abs(rec.normal.dot(edgeDirectionX)) > 0.9)
+        edgeDirectionX = Math::Vector3D<double>(0, 0, 1);
+
+    Math::Vector3D<double> edgeDirectionY =
+        Math::Vector3D<double>(
+            rec.normal._y * edgeDirectionX._z - rec.normal._z * edgeDirectionX._y,
+            rec.normal._z * edgeDirectionX._x - rec.normal._x * edgeDirectionX._z,
+            rec.normal._x * edgeDirectionX._y - rec.normal._y * edgeDirectionX._x)
+            .normalized();
+    edgeDirectionX = Math::Vector3D<double>(
+                         rec.normal._y * edgeDirectionY._z - rec.normal._z * edgeDirectionY._y,
+                         rec.normal._z * edgeDirectionY._x - rec.normal._x * edgeDirectionY._z,
+                         rec.normal._x * edgeDirectionY._y - rec.normal._y * edgeDirectionY._x)
+                         .normalized();
+    rec.u = rec.p.dot(edgeDirectionX) * 0.1;
+    rec.v = rec.p.dot(edgeDirectionY) * 0.1;
+
     rec.material = _material.get();
 
     return true;
