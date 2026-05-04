8dd4c5e941c4f2d9c50a6f95689d976d1c486e56
Nielsen Combe-Bracciale <nielsen.combe-bracciale@epitech.eu>
2026-05-04 15:08:56 +0200

    refactor(MaterialUtils): replace randomDouble implementation with a more robust method using uniform distribution

--- include/Materials/MaterialUtils.hpp
+++ include/Materials/MaterialUtils.hpp
@@ -17,7 +17,12 @@
 namespace RayTracer {
     class MaterialUtils {
         public:
-        static double randomDouble() { return rand() / (RAND_MAX + 1.0); }
+        static double randomDouble()
+        {
+            static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+            static std::mt19937 generator;
+            return distribution(generator);
+        }
 
         static double schlick(double cosine, double ior)
         {
