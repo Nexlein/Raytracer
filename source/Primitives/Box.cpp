b369763947c1519704894f58645f22fcb8cfb179
Yumi-alt <julie.vincent@epitech.eu>
2026-05-04 14:11:38 +0200

    feat: add rotation to the box

--- source/Primitives/Box.cpp
+++ source/Primitives/Box.cpp
@@ -20,25 +20,53 @@ namespace RayTracer {
     {
         rec.material = _material.get();
 
-        double txmin = (_min._x - ray._origin._x) / ray._direction._x;
-        double txmax = (_max._x - ray._origin._x) / ray._direction._x;
-        double tymin = (_min._y - ray._origin._y) / ray._direction._y;
-        double tymax = (_max._y - ray._origin._y) / ray._direction._y;
-        double tzmin = (_min._z - ray._origin._z) / ray._direction._z;
-        double tzmax = (_max._z - ray._origin._z) / ray._direction._z;
+        Math::Point3D<double> center = {
+            (_min._x + _max._x) * 0.5,
+            (_min._y + _max._y) * 0.5,
+            (_min._z + _max._z) * 0.5
+        };
+
+        Math::Vector3D<double> localOrigin = {
+            ray._origin._x - center._x,
+            ray._origin._y - center._y,
+            ray._origin._z - center._z
+        };
+        Math::Vector3D<double> localDir = ray._direction;
+
+        localOrigin.rotateZ(-_rotation._z);
+        localOrigin.rotateY(-_rotation._y);
+        localOrigin.rotateX(-_rotation._x);
+
+        localDir.rotateZ(-_rotation._z);
+        localDir.rotateY(-_rotation._y);
+        localDir.rotateX(-_rotation._x);
+
+        Math::Point3D<double> localMin = {
+            _min._x - center._x, _min._y - center._y, _min._z - center._z
+        };
+        Math::Point3D<double> localMax = {
+            _max._x - center._x, _max._y - center._y, _max._z - center._z
+        };
+
+        double txmin = (localMin._x - localOrigin._x) / localDir._x;
+        double txmax = (localMax._x - localOrigin._x) / localDir._x;
+        double tymin = (localMin._y - localOrigin._y) / localDir._y;
+        double tymax = (localMax._y - localOrigin._y) / localDir._y;
+        double tzmin = (localMin._z - localOrigin._z) / localDir._z;
+        double tzmax = (localMax._z - localOrigin._z) / localDir._z;
 
         double txEntry = std::fmin(txmin, txmax);
         double tyEntry = std::fmin(tymin, tymax);
         double tzEntry = std::fmin(tzmin, tzmax);
 
-        double tmin = std::fmax(std::fmax(txEntry, tyEntry), tzEntry);
-        double tmax = std::fmin(
-            std::fmin(
-                std::fmax(txmin, txmax),
-                std::fmax(tymin, tymax)
-            ),
-            std::fmax(tzmin, tzmax)
-        );
+        int face = 0;
+        double tmin = txEntry;
+        if (tyEntry > tmin) { tmin = tyEntry; face = 1; }
+        if (tzEntry > tmin) { tmin = tzEntry; face = 2; }
+
+        double tmax = std::fmin(std::fmin(std::fmax(txmin, txmax),
+                                        std::fmax(tymin, tymax)),
+                                        std::fmax(tzmin, tzmax));
 
         if (tmax <= tmin) return false;
 
@@ -46,15 +74,15 @@ namespace RayTracer {
         if (rec.distance < 0) return false;
 
         Math::Vector3D<double> localNormal;
-        if (tmin == txEntry)
-            localNormal = (ray._direction._x < 0) ? Math::Vector3D<double>{1,0,0} :
-                                                    Math::Vector3D<double>{-1,0,0};
-        else if (tmin == tyEntry)
-            localNormal = (ray._direction._y < 0) ? Math::Vector3D<double>{0,1,0} :
-                                                    Math::Vector3D<double>{0,-1,0};
+        if (face == 0)
+            localNormal = (localDir._x < 0) ? Math::Vector3D<double>{1,0,0} :
+                                              Math::Vector3D<double>{-1,0,0};
+        else if (face == 1)
+            localNormal = (localDir._y < 0) ? Math::Vector3D<double>{0,1,0} :
+                                              Math::Vector3D<double>{0,-1,0};
         else
-            localNormal = (ray._direction._z < 0) ? Math::Vector3D<double>{0,0,1} :
-                                                    Math::Vector3D<double>{0,0,-1};
+            localNormal = (localDir._z < 0) ? Math::Vector3D<double>{0,0,1} :
+                                              Math::Vector3D<double>{0,0,-1};
 
         localNormal.rotateX(_rotation._x);
         localNormal.rotateY(_rotation._y);
